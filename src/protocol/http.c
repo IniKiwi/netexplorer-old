9c49a838258e8dddcc84d13592f5fd7f4c292f00
IniKiwi <johndoe@example.com>
2023-02-12 18:11:10 +0100

    http protocol: separate request in a new fonction

--- src/protocol/http.c
+++ src/protocol/http.c
@@ -2,41 +2,42 @@
 
 #define HTTP_RECV_BUFFER 10000
 
-http_server_info_t protocol_get_http_server_info(int sockfd, port_t addr, struct network_task_info info, char* msg){
+int http_get(int sockfd, port_t addr, struct network_task_info info, char* buffer, size_t buffer_size , const char* path){
     const char* http_req = "GET /path?query_string HTTP/1.0\r\n\r\n";
     size_t http_req_s = strlen(http_req);
-    char* http_ret = malloc(HTTP_RECV_BUFFER);
+    char* http_ret = buffer;
 
     int sent = 0;
     while (sent < http_req_s){
         int bytes = send(sockfd,http_req+sent,http_req_s-sent, 0);
         if (bytes < 0)
-            return (http_server_info_t){.status = STATUS_FAIL};
+            return 0;
         if (bytes == 0)
             break;
         sent+=bytes;
     };
 
-    bzero(http_ret, HTTP_RECV_BUFFER);
+    bzero(http_ret, buffer_size);
 
-    int total = HTTP_RECV_BUFFER-1;
+    int total = buffer_size-1;
     int received = 0;
     while (received < total){
         int bytes = recv(sockfd,http_ret+received,total-received, 0);
         if (bytes < 0)
-            return (http_server_info_t){.status = STATUS_FAIL};
+            return 0;
         if (bytes == 0)
             break;
-        //fwrite(http_ret+received, 1, bytes, stdout);
-        //printf("\n======================\n");
         received+=bytes;
     };
-    http_server_info_t ret_info;
-    ret_info.status = STATUS_OK;
+    return received;
+}
 
-    //printf(http_ret);
+http_server_info_t protocol_get_http_server_info(int sockfd, port_t addr, struct network_task_info info, char* msg){
+    char* http_ret = malloc(HTTP_RECV_BUFFER);
 
-    int http_ret_l = strlen(http_req);
+    http_get(sockfd, addr, info, http_ret, HTTP_RECV_BUFFER, "/");
+
+    int http_ret_l = strlen(http_ret);
     int http_ret_wl = 0;
     for(int i=0;i<HTTP_RECV_BUFFER;i++){
         if(http_ret[i] == ' ' || http_ret[i] == '\n' || http_ret[i] == '\r' || http_ret[i] == ';'){
@@ -44,6 +45,9 @@ http_server_info_t protocol_get_http_server_info(int sockfd, port_t addr, struct
             http_ret_wl++;
         }
     }
+
+    http_server_info_t ret_info;
+    ret_info.status = STATUS_OK;
 
     char* http_ret_i = http_ret;
     http_ret_i += strlen(http_ret_i)+1;
